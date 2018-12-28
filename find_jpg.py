4505f99fe86e748c2dceb1319468eefc2320a144
Ilya Lavrinov <ilya.v.lavrinov@gmail.com>
2018-12-28 20:02:57 +0300

    find_jpg big try-catch

--- find_jpg.py
+++ find_jpg.py
@@ -15,29 +15,30 @@ def find_jpg_with_creation_date(folder, date_from, date_to, copy_to):
             fpath = os.path.join(root, f)
             try:
                 img = PIL.Image.open(fpath)
-            except:
-                continue
-            if img.format != "JPEG":
-                continue
-            exifdata = img._getexif()
-            if exifdata is None:
-                continue
-            exif = {
-                PIL.ExifTags.TAGS[k]: v
-                for k, v in exifdata.items()
-                if k in PIL.ExifTags.TAGS
-            }
-            if "DateTime" not in exif:
-                continue
-            exif_date_str = exif["DateTime"]
-            exif_date = datetime.datetime.strptime(exif_date_str, "%Y:%m:%d %H:%M:%S")
-            if exif_date >= date_from and exif_date < date_to:
-                print "Found suitable file at '{}'".format(fpath)
-                result.append(fpath)
-                to_fpath = os.path.join(copy_to, f)
-                if os.path.exists(to_fpath):
+                if img.format != "JPEG":
                     continue
-                shutil.copyfile(fpath, to_fpath)
+                exifdata = img._getexif()
+                if exifdata is None:
+                    continue
+                exif = {
+                    PIL.ExifTags.TAGS[k]: v
+                    for k, v in exifdata.items()
+                    if k in PIL.ExifTags.TAGS
+                }
+                if "DateTime" not in exif:
+                    continue
+                exif_date_str = exif["DateTime"]
+                exif_date = datetime.datetime.strptime(exif_date_str, "%Y:%m:%d %H:%M:%S")
+                if exif_date >= date_from and exif_date < date_to:
+                    print "Found suitable file at '{}'".format(fpath)
+                    result.append(fpath)
+                    to_fpath = os.path.join(copy_to, f)
+                    if os.path.exists(to_fpath):
+                        continue
+                    shutil.copyfile(fpath, to_fpath)
+            except Exception as e:
+                print "Exception caught: {}".format(e)
+                continue
 
         #for d in dirnames:
         #    result += find_jpg_with_creation_date(os.path.join(dirname, d), date_from, date_to)
